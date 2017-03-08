adac123922b2a1b0fc7476a2c8a7eb01958228be
Paul Lim <plim0793@calvisitor-10-105-185-70.calvisitor.1918.berkeley.edu>
2017-03-07 16:38:38 -0800

    edited script

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,32 @@
 # Hint:  use Google to find python function
 
-####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+from datetime import date, datetime
 
-####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+def main():
 
-####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+	####a) 
+	date_format = '%m-%d-%Y'
+	date_start = datetime.strptime('01-02-2013', date_format)  
+	date_stop = datetime.strptime('07-28-2015', date_format)   
+	delta_a = date_stop - date_start
+	print(delta_a.days)
+
+	####b)  
+	date_format = '%m%d%Y'
+	date_start = datetime.strptime('12312013', date_format)
+	date_stop = datetime.strptime('05282015', date_format)
+	delta_b = date_stop - date_start
+	print(delta_b.days)
+
+	####c)  
+	date_format = '%d-%b-%Y'
+	date_start = datetime.strptime('15-Jan-1994', date_format)
+	date_stop = datetime.strptime('14-Jul-2015', date_format)
+	delta_c = date_stop - date_start
+	print(delta_c.days)
+
+
+
+if __name__ == '__main__':
+	main()
+
