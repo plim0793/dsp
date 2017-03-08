12bb0683f5f33184c8e3edaf9f20ce31cd4a04e6
Paul Lim <plim0793@Pauls-MacBook-Pro.local>
2017-03-08 13:29:49 -0800

    answered questions

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,15 @@
-PLACE YOUR CODE HERE
+import pandas as pd
+
+
+
+def main():
+	df = pd.read_csv('faculty.csv')
+	print(df.columns.values)
+
+	df_emails = pd.DataFrame()
+	df_emails['email'] = df[' email']
+
+	df_emails.to_csv('emails.csv')
+
+if __name__ == '__main__':
+	main()
